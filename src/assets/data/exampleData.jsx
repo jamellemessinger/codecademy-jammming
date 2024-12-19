c8b0807b91bfd8b2195a3e1027eeb8263d8e860c
Jamelle Messinger <jamswoof914@gmail.com>
2024-12-19 10:18:26 -0700

    add objects containing mock data to tracks array in order to test component functionality

--- src/assets/data/exampleData.jsx
+++ src/assets/data/exampleData.jsx
@@ -1,1 +1,19 @@
-const tracks = [{}];
+const tracks = [
+  {
+    songTitle: 'All Of Me',
+    artist: 'John Legend',
+    album: 'Love In The Future',
+  },
+  {
+    songTitle: 'TANTRUM',
+    artist: 'Will Smith, Joyner Lucas',
+    album: 'TANTRUM',
+  },
+  {
+    songTitle: 'Will',
+    artist: 'Joyner Lucas',
+    album: 'Will',
+  },
+];
+
+export default tracks;
