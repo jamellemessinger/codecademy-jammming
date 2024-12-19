aa1fe8053368b61f3bb70ce115c1888f16e036c1
Jamelle Messinger <jamswoof914@gmail.com>
2024-12-19 11:14:59 -0700

    implement Track component to hold all the data for each search result that comes back

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Track() {
-  return <></>;
+export default function Track({songTitle, artist, album}) {
+  const [inPlaylist, setInPlayList] = useState(false);
+
+  const handleOnClick = () => {
+    inPlaylist ? setInPlayList(false) : setInPlayList(true);
+  };
+
+  return (
+    <>
+      <div className="track-container">
+        <div>
+          <p>{songTitle}</p>
+          <p>{artist} | {album}</p>
+        </div>
+        <div>
+          <button onClick={handleOnClick}>
+            {!inPlaylist ? 'add' : 'remove'}
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
