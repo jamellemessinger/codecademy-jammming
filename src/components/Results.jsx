a4759af2a69ba62b6977957ac862904910125f81
Jamelle Messinger <jamswoof914@gmail.com>
2024-12-19 12:26:45 -0700

    add a unique key prop to each instance of the Track component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Track from './Track';
-import tracks from '../assets/data/exampleData'
+import tracks from '../assets/data/exampleData';
 
 export default function Results() {
-  
   return (
     <>
       <p>Results</p>
-      {tracks.map((track) => <Track songTitle={track.songTitle} artist={track.artist} album={track.album} />)}
+      {tracks.map((track, index) => (
+        <Track
+          key={index}
+          songTitle={track.songTitle}
+          artist={track.artist}
+          album={track.album}
+        />
+      ))}
     </>
   );
 }
